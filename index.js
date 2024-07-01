6cb2e2f9ece77f99fa4ad4ea8c6b40b5467059c6
Harshil Rao <harshilraoca@outlook.com>
2024-07-01 19:54:36 -0400

    Stage 1: Home Page, Navigation, Footer

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve static files from the "public" directory
+app.use('/public', express.static('public'));
+
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hello World!' });
+    res.render('index', { title: 'Hello World!' });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
