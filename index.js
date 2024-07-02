bf942b18f41dd2d084f0537c45a77a92a06eb583
Harshil Rao <harshilraoca@outlook.com>
2024-07-01 20:50:23 -0400

    editing files, video add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hello World!' });
+    res.render('home', { title: 'Home' });
 });
 
 app.listen(port, () => {
